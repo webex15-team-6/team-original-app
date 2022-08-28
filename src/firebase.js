22984980760f3c2947e609956f21bfc0a3e1bb12
yudai-yamaguchi <yudaiyama10@gmail.com>
2022-08-28 21:16:58 +0900

    初回のコミット

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,15 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: "AIzaSyAU3N9zyYwVxqpIrU9cChlWmHa-mExXqVQ",
+  authDomain: "team6-development.firebaseapp.com",
+  projectId: "team6-development",
+  storageBucket: "team6-development.appspot.com",
+  messagingSenderId: "197542973591",
+  appId: "1:197542973591:web:c66d68bbebacdf852719d2",
+  measurementId: "G-1E0Q58Y8VX",
+}
 
 const app = initializeApp(firebaseConfig)
 
